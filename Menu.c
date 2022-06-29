a24b7c8862d2789e754c62ac0ea1bf88c56975ec
freksel <alejandro.marin.s@mail.pucv.cl>
2022-06-28 20:20:41 -0400

    mejora del menu

--- Menu.c
+++ Menu.c
@@ -8,25 +8,26 @@ void main(){
 
     List* ListaPuntaje=createList();
     int eleccion;
+    int eleccionf;
     while(eleccion!=0){
         printf("======================================================================\n");
         printf("H                                                                    H\n");
-        printf("H                            TETRIS                                  H\n");
-        printf("H                             PUCV                                   H\n");
+        printf("H                          T E T R I S                               H\n");
+        printf("H                           P U C V                                  H\n");
         printf("H                                                                    H\n");
         printf("H                                                                    H\n");
-        printf("H                            1-.Iniciar juego                        H\n");
-        printf("H                            2-%cQue es el tetris?                    H\n",168);
-        printf("H                            3-.Puntajes                             H\n");
+        printf("H                           1-.Iniciar juego                         H\n");
+        printf("H                           2-Comandos                               H\n");
+        printf("H                           3-.Puntajes                              H\n");
         printf("H                                                                    H\n");
-        printf("H                            0-.Cerrar juego                         H\n");
+        printf("H                           0-.Cerrar juego                          H\n");
         printf("======================================================================\n");
         //printf("1-.Iniciar juego\n");
         //printf("2-¿Que es el tetris?\n");
         //printf("0-.Cerrar juego\n");
 
         scanf("%d",&eleccion);
-
+        system("cls");
         switch(eleccion){
             case 1:
                 system("cls");
@@ -46,10 +47,25 @@ void main(){
                     break;
                 }
             break;
+            case 2:
+                system("cls");
+                printf("=================================\n");
+                printf("H   Mover : Izquierda/Derecha   H\n");
+                printf("H   Rotar : Arriba              H\n");
+                printf("H   Acelerar : Abajo            H\n");
+                printf("H   Espacio : Bajar Tetromino   H\n");
+                printf("H   S : Pausa                   H\n");
+                printf("=================================\n");
+                printf("\n=============================================\n");
+                printf("H          PRESIONE CUALQUIER TECLA        H\n");
+                printf("=============================================\n");
+                scanf("%d",&eleccionf);
+                system("cls");
+                break;               
             case 3:
+            system("cls");
             mostrarPuntajes(ListaPuntaje);
             break;
-
         }
     }
 }
